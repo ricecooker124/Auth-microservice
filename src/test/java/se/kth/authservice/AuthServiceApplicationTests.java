8fa2a19979486bf9977c95ae0cb74d7b7bc04057
Simon Alam <simonalam2003@gmail.com>
2025-12-04 01:44:15 +0100

    Initial commit for auth-service

---
+++ src/test/java/se/kth/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.kth.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
